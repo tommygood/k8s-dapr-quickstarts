87c2fbb8a86796ae12c08a92a06483ed167bdc63
tommygood <tommy50508@gmail.com>
2025-05-15 08:57:18 +0000

    feat: adding the obsererability in service invocation.

--- service-invocation/server/index.js
+++ service-invocation/server/index.js
@@ -1,11 +1,32 @@
 import express from 'express';
+import axios from 'axios';
 
 const app = express();
 app.use(express.json());
 
-app.post('/orders', (req, res) => {
-    console.log("Order received:", req.body);
-    res.sendStatus(200);
+async function pub(traceparent) {
+	const payload = {
+	  orderId: Math.floor(Math.random() * 10000),
+	  item: "bananaQQ"
+	};
+
+	const headers = traceparent ? { traceparent } : {};
+
+	axios.post(`http://localhost:3500/v1.0/publish/redis-pubsub/orders`, payload, { headers })
+	.then(() => console.log("Published order:", payload))
+	.catch((err) => console.error("Error publishing:", err.message));
+}
+
+app.post('/orders', async (req, res) => {
+	console.log("Order received:", req.body);
+	const traceparent = req.headers['traceparent']; // ← capture tracing header
+	await pub(traceparent);
+	res.sendStatus(200);
+});
+
+app.post('/qq', async (req, res) => {
+	console.log("Order received:", req.body);
+	res.sendStatus(500);
 });
 
 app.listen(5001);
